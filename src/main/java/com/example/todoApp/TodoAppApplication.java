0027bcce72e1c8b121083c97604d4837679dbb0c
Igor Kuruc <krc13@email.cz>
2025-04-01 13:17:47 +0200

    deleting unused imports

--- src/main/java/com/example/todoApp/TodoAppApplication.java
+++ src/main/java/com/example/todoApp/TodoAppApplication.java
@@ -1,13 +1,7 @@
 package com.example.todoApp;
 
-import com.example.todoApp.todo.Item;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class TodoAppApplication {
