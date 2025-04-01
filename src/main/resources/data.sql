719a1405635d63e0705fdb94fd6ff843fe2e4a75
Igor Kuruc <krc13@email.cz>
2025-04-01 17:51:37 +0200

    adjusting sql in data.sql to not run every rerun of the app

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO item ( title, created, adjusted, task) VALUES ('testing', '2024-02-22T12:16:00.000000',null,'testing stuff out');
+INSERT INTO item (title, created, adjusted, task) SELECT 'testing', '2024-02-22T12:16:00.000000',null,'testing stuff out'
+WHERE NOT EXISTS (SELECT 'testing' FROM item WHERE title = 'testing')
